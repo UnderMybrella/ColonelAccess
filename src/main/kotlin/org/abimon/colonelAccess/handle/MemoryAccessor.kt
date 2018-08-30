b2f0de5a9c018beef0537d1b0e5e790ef5201f42
Isaac <undermybrella@abimon.org>
2018-08-30 13:59:56 +1000

    Add: MemoryAccessor#readInts

--- src/main/kotlin/org/abimon/colonelAccess/handle/MemoryAccessor.kt
+++ src/main/kotlin/org/abimon/colonelAccess/handle/MemoryAccessor.kt
@@ -5,17 +5,16 @@ import org.abimon.colonelAccess.osx.OSXMemoryAccessor
 import org.abimon.colonelAccess.win.WindowsMemoryAccessor
 import java.util.*
 
-
-
 abstract class MemoryAccessor<out E>(pid: Int) {
     abstract fun readMemory(address: Long, size: Int): Pair<Pointer?, E?>
 
     fun readInt(address: Long): Pair<Int?, E?> {
         val (memory, error) = readMemory(address, 4)
-        if (memory != null)
-            return memory.getInt(0) to error
-        return memory to error
+        return memory?.getInt(0) to error
     }
+
+    fun readInts(vararg addresses: Long): Map<Long, Pair<Int?, E?>> =
+            addresses.map { addr -> addr to readInt(addr) }.toMap()
 
     companion object {
         fun accessorForSystem(pid: Int): MemoryAccessor<*> {
